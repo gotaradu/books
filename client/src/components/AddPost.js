03a5ef6e74a1d1579b25b9372b34cf8606e78c5b
Radu <radugota6@gmail.com>
2023-03-06 21:30:12 +0200

    h-fit for description container

--- client/src/components/AddPost.js
+++ client/src/components/AddPost.js
@@ -2,9 +2,9 @@ const styles = { label: 'mr-4' }
 
 const AddPost = () => {
   return (
-    <div className="m-auto mt-3 mb-3 text-center h-fit p-3 border-solid border-verzisor border-4 flex-col max-sm:h-44">
+    <div className="m-auto mt-3 mb-3 text-center h-fit p-3 border-solid border-verzisor border-4 flex-col max-sm:h-fit">
       <div>
-        <label className="mr-4 text-4xl w-max max-sm:text-xl">Title</label>
+        <label className="mr-4 text-4xl w-fit max-sm:text-xl">Title</label>
         <textarea
           className="w-full h-auto text-4xl "
           cols="1"
